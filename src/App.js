48a25fa22d6bd79dd122152f9123f5f8803902d6
cd ~/code/$deyvisong/dotfiles && zsh git_setup.sh <deyvisonalves@protonmail.com>
2024-12-11 23:18:11 +0100

    CSS inline

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import logo from "./logo.svg";
 import "./App.css";
+import MyComponent from "./components/MyComponent";
 
 function App() {
   return (
     <div className="App">
+      {/* CSS Global */}
       <h1>React with CSS</h1>
+      {/* Component CSS */}
+      <MyComponent />
+      <p className="my-comp-paragraph">
+        This paragraph belongs to MyComponent.js{" "}
+      </p>
+      <p>This paragraph belongs to App.js </p>
+      {/* Inline CSS */}
+      <p
+        style={{ color: "blue", padding: "25px", borderTop: "2px  solid red" }}
+      >
+        This element was styled inline
+      </p>
+      <p
+        style={{
+          color: "magenta",
+          padding: "25px",
+          borderTop: "2px  solid red",
+        }}
+      >
+        This element was styled inline
+      </p>
     </div>
   );
 }
---
+++ src/components/MyComponent.js
@@ -0,0 +1,11 @@
+import "./MyComponent.css";
+const MyComponent = () => {
+  return (
+    <div>
+      <h1>CSS of component</h1>
+      <p>This is a paragraph of component</p>
+    </div>
+  );
+};
+
+export default MyComponent;
